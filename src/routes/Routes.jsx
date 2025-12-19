c65851cfa462b7c6dff69c3f50b07db2af795593
Antonio <antonio13050@gmail.com>
2025-12-18 21:20:57 -0300

    fix: remove Leads

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -9,7 +9,6 @@ import ImovelDetalhes from "@/pages/ImovelDetalhes";
 import Clientes from "@/pages/Clientes";
 import Corretores from "@/pages/Corretores";
 import RelatorioAdmin from "@/pages/RelatorioAdmin";
-import Leads from "@/pages/Leads";
 import ClienteImoveisPersonalizados from "@/pages/ClienteImoveisPersonalizados";
 import ClienteDetalhes from "@/pages/ClienteDetalhes";
 import ImobiliariaPage from "@/pages/Imobiliaria";
@@ -20,51 +19,44 @@ import ImovelNovo from "@/pages/ImovelNovo";
 import ImovelEditar from "@/pages/ImovelEditar";
 
 export default function RoutesComponent() {
-    return (
-        <Routes>
-            {/* Rotas protegidas com Layout */}
-            <Route
-                path="/"
-                element={
-                    <PrivateRoutes>
-                        <Layout />
-                    </PrivateRoutes>
-                }
-            >
-                <Route index element={<Dashboard />} />
-                <Route path="dashboard" element={<Dashboard />} />
-                <Route path="imoveis" element={<Imoveis />} />
-                <Route path="imoveis/novo" element={<ImovelNovo />} />
-                <Route path="imoveis/:id/editar" element={<ImovelEditar />} />
-                <Route path="leads" element={<Leads />} />
-                <Route path="clientes" element={<Clientes />} />
-                <Route path="corretores" element={<Corretores />} />
-                <Route path="mapa" element={<Mapa />} />
-                <Route path="imobiliaria" element={<ImobiliariaPage />} />
-                <Route path="relatorio-admin" element={<RelatorioAdmin />} />
-                <Route path="processos" element={<Processos />} />
-                <Route path="visitas" element={<Visitas />} />
-                <Route path="agenda" element={<Agenda />} />
+  return (
+    <Routes>
+      {/* Rotas protegidas com Layout */}
+      <Route
+        path="/"
+        element={
+          <PrivateRoutes>
+            <Layout />
+          </PrivateRoutes>
+        }
+      >
+        <Route index element={<Dashboard />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="imoveis" element={<Imoveis />} />
+        <Route path="imoveis/novo" element={<ImovelNovo />} />
+        <Route path="imoveis/:id/editar" element={<ImovelEditar />} />
+        <Route path="clientes" element={<Clientes />} />
+        <Route path="corretores" element={<Corretores />} />
+        <Route path="mapa" element={<Mapa />} />
+        <Route path="imobiliaria" element={<ImobiliariaPage />} />
+        <Route path="relatorio-admin" element={<RelatorioAdmin />} />
+        <Route path="processos" element={<Processos />} />
+        <Route path="visitas" element={<Visitas />} />
+        <Route path="agenda" element={<Agenda />} />
 
-                <Route
-                    path="imovel-detalhes/:id"
-                    element={<ImovelDetalhes />}
-                />
-                <Route
-                    path="cliente-detalhes/:id"
-                    element={<ClienteDetalhes />}
-                />
-                <Route path="*" element={<h1>Page not found</h1>} />
-            </Route>
-            {/* Rota de login sem Layout */}
-            <Route path="login" element={<Login />} />
-            {/* Página personalizada de imóveis por cliente (pública) */}
-            <Route
-                path="cliente-imoveis/:id"
-                element={<ClienteImoveisPersonalizados />}
-            />
-            {/* Rota para páginas não encontradas */}
-            <Route path="*" element={<h1>Page not found</h1>} />
-        </Routes>
-    );
+        <Route path="imovel-detalhes/:id" element={<ImovelDetalhes />} />
+        <Route path="cliente-detalhes/:id" element={<ClienteDetalhes />} />
+        <Route path="*" element={<h1>Page not found</h1>} />
+      </Route>
+      {/* Rota de login sem Layout */}
+      <Route path="login" element={<Login />} />
+      {/* Página personalizada de imóveis por cliente (pública) */}
+      <Route
+        path="cliente-imoveis/:id"
+        element={<ClienteImoveisPersonalizados />}
+      />
+      {/* Rota para páginas não encontradas */}
+      <Route path="*" element={<h1>Page not found</h1>} />
+    </Routes>
+  );
 }
